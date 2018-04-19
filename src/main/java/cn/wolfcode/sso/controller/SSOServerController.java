e56a45f2005c9ed205f8038e204b11aa0b68bb77
lanxw <lanxiongwei@wolfcode.cn>
2018-04-19 15:04:07 +0800

    阶段二: 1.添加MockDataBaseUtil.java文件模拟数据库的t_token表 2.在SSOServerController中完成登陆方法逻辑 3.在SSOServerController添加verifyToken校验令牌信息方法

--- src/main/java/cn/wolfcode/sso/controller/SSOServerController.java
+++ src/main/java/cn/wolfcode/sso/controller/SSOServerController.java
@@ -1,11 +1,14 @@
 package cn.wolfcode.sso.controller;
 
+import cn.wolfcode.sso.util.MockDatabaseUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 /**
  * Created by wolfcode-lanxw
@@ -35,5 +38,49 @@ public class SSOServerController {
             //目前这段逻辑我们先不写,按着图解流程编写代码
             return "";
         }
+    }
+
+    /**
+     * 登陆方法
+     * @param username      前台登陆的用户名
+     * @param password      前台登陆的密码
+     * @param redirectUrl   客户端被拦截的地址
+     * @param session       服务端会话对象
+     * @param model         模型数据
+     * @return               响应的视图地址
+     */
+    @RequestMapping("/login")
+    public String login(String username,String password,String redirectUrl,HttpSession session,Model model){
+        if("zhangsan".equals(username)&&"666".equals(password)){
+            //账号密码匹配
+            //1.创建令牌信息,只要保证唯一即可,我们就使用UUID.
+            String token = UUID.randomUUID().toString();
+            //2.创建全局的会话,把令牌信息放入会话中.
+            session.setAttribute("token",token);
+            //3.需要把令牌信息放到数据库中.
+            MockDatabaseUtil.T_TOKEN.add(token);
+            //4.重定向到redirectUrl,把令牌信息带上.  http://www.crm.com:8088/main?token=
+            model.addAttribute("token",token);
+            return "redirect:"+redirectUrl;
+        }
+        //如果账号密码有误,重新回到登录页面,还需要把redirectUrl放入request域中.
+        model.addAttribute("redirectUrl",redirectUrl);
+        return "login";
+    }
+
+    /**
+     * 校验客户端传过来的令牌信息是否有效
+     * @param token 客户端传过来的令牌信息
+     * @return
+     */
+    @RequestMapping("/verify")
+    @ResponseBody
+    public String verifyToken(String token){
+        //在模拟的数据库表t_token中查找是否有这条记录
+        if(MockDatabaseUtil.T_TOKEN.contains(token)){
+            //说明令牌有效,返回true
+            return "true";
+        }
+        return "false";
     }
 }
---
+++ src/main/java/cn/wolfcode/sso/util/MockDatabaseUtil.java
@@ -0,0 +1,9 @@
+package cn.wolfcode.sso.util;
+import java.util.*;
+/**
+ * Created by wolfcode-lanxw
+ */
+public class MockDatabaseUtil {
+    //模拟数据库中的t_token表
+    public static Set<String> T_TOKEN = new HashSet<String>();
+}
